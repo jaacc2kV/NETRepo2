4bc5b1486528724dd2854f92fca1f144bbff5677
Jorge C <jaczperu@gmail.com>
2026-04-08 00:45:37 -0500

    Primer push con estructura base de proyecto y configuración de gitignore

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Console.WriteLine("Hello, World!");
+
+using System.Reflection;
+
+String pruebavariable = "Soy Variable";
+
+Assembly assembly = Assembly.GetExecutingAssembly();
+Version? version = assembly.GetName().Version;
+
+
+Console.WriteLine("=========================================");
+Console.WriteLine("     SISTEMA DE GESTIÓN DE INVENTARIO    ");
+Console.WriteLine("=========================================");
+Console.WriteLine();
+Console.WriteLine($"Versión: {version}]");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");      //INTERPOLACION: $"....{variable}"
+Console.WriteLine($".NET Versión: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine($"Prueba Variable: {pruebavariable}");        //INTERPOLACION: $"....{variable}"
+Console.WriteLine("Estructura del proyecto");
+Console.WriteLine("Configuración .csproj");
+Console.WriteLine("Carpeta src/ creada");
+Console.WriteLine("Metadatos configurados");
+Console.WriteLine();
+Console.WriteLine("Estado: Proyecto inicializado");
